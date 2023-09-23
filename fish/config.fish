49d8b07a8d1a373dd3788256e696431a3a2c7ad9
IoaNNUwU <ioannxd@gmail.com>
2023-09-23 19:38:54 +0200

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
 ﻿if status is-interactive
-    alias idea="~/apps/idea/bin/idea.sh &"
-    alias gpu="nvtop"
-    starship init fish | source
+	
+	alias rust="cd ~/projects/rust"
+	alias idea="~/apps/idea/bin/idea.sh &"
+	alias gpu="nvtop"
+	starship init fish | source
 end
 
 set fish_greeting ""
